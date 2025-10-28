125b12f8d89dc8cde7dc41ba55c80153b577a687
Khadija Elabbioui <khadijaelabbioui1999@gmail.com>
2025-10-28 11:56:03 +0100

    new file:   best_pipeline_XGBoost.pkl 	modified:   pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -4,14 +4,18 @@ from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC 
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix ,f1_score, roc_auc_score
 from xgboost import XGBClassifier
 from Functions import Charge_data, clean_data, Encode_df, split_data
+
+import joblib
+
+
 
 
 
 #  Charger et préparer les données 
-df = Charge_data('heart_disease_dataset.csv')
+df = Charge_data(r"C:\Users\khadija\Desktop\API_machine_learning-1\Cardiovasculaires_Data.csv")
 df = clean_data(df)
 df = Encode_df(df, col='status')
 X_train, X_test, y_train, y_test = split_data(df, 'status')
@@ -27,11 +31,13 @@ preprocessor = ColumnTransformer(transformers=[
 # les modèles
 models = {
 'RandomForest': RandomForestClassifier(random_state=42),
-'SVC': SVC(probability=True, random_state=42),
-'LogisticRegression': LogisticRegression(max_iter=1000, random_state=42),
-'XGBoost': XGBClassifier(use_label_encoder=False, eval_metric='logloss', random_state=42)
+'XGBoost': XGBClassifier(random_state=42)
 }
 # --- Entraînement et évaluation pour chaque modèle ---
+best_model = None
+best_score = 0
+best_name = ""
+
 for name, model in models.items():
     print(f"\n--- {name} ---")
     clf = Pipeline(steps=[
@@ -40,5 +46,26 @@ for name, model in models.items():
       ])
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
-    print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
-    print("Classification Report:\n", classification_report(y_test, y_pred))
+    roc = roc_auc_score(y_test, y_pred)
+    print( 'ROC_AUC score : %.3f' % roc )
+    F1 = f1_score(y_test, y_pred, average='binary')
+    print('F1_Score: %.3f' % F1)
+
+    # Sauvegarder le meilleur modèle selon F1
+    if F1 > best_score:
+        best_score = F1
+        best_model = clf
+        best_name = name
+
+
+print(f"\n Meilleur modèle : {best_name} avec F1 = {best_score:.3f}")
+
+joblib.dump(best_model, f"best_pipeline_{best_name}.pkl")
+print(f"Modele sauvegardé '")
+
+# Charger le modèle sauvegardé
+
+model_loaded = joblib.load("best_pipeline_XGBoost.pkl")
+
+# Tester sur de nouvelles données
+y_pred_test = model_loaded.predict(X_test)
