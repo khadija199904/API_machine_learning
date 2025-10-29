62029b8c65146d6c39c20369051a2cebf5bb78e6
ismailelghazi <ismail47el@gmail.com>
2025-10-30 00:13:46 +0100

    lets goo

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,6 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Cardio Risk API"
     DATABASE_URL: str = "sqlite:///./cardio.db"
-
     class Config:
         env_file = ".env"
 
