635e60a322d182f6a85f61291a2d45e889368231
Khadija Elabbioui <khadijaelabbioui1999@gmail.com>
2025-10-31 10:42:11 +0100

    edit test.py

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import pytest 
-
-from pipeline import df
-
+from Functions import Charge_data 
 
 
-def test_load_data(df):
-    
+
+def test_load_data():
+    filepath = "Cardiovasculaires_Data.csv"
+    df = Charge_data(filepath)
     assert not df.empty
     print("\n Le dataset est chargé avec succès !")
