5ae7ff6f80fd1821a8527becd7e8cf84cade23bf
Khadija Elabbioui <khadijaelabbioui1999@gmail.com>
2025-10-31 10:25:54 +0100

    edit

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 import pytest 
 from Functions import Charge_data 
+from pipeline import df
 import pandas as pd
 
 
-def test_load_data():
-    filepath = r"C:\Users\khadija\Desktop\API_machine_learning-1\Cardiovasculaires_Data.csv"
-    df = Charge_data(filepath)
+def test_load_data(df):
+    
+    
     assert isinstance(df, pd.DataFrame)
     assert not df.empty
     print("\n Le dataset est chargé avec succès !")
